f7484c6d1dc64826bf19774cd9c70fc53b18654f
huangl <supershim@icloud.com>
2026-03-01 18:28:49 +0100

    ajouter la source de gdp
    telecharment dans une nouvelle dataframe

--- produtivity_chomage.R
+++ produtivity_chomage.R
@@ -5,6 +5,11 @@ productivity <-  read.csv(url_productivity) |> dplyr::select(entity, year,produc
 url_unemployment <- "https://ourworldindata.org/grapher/unemployment-rate.csv?v=1&csvType=full&useColumnShortNames=true"
 unemployment <-  read.csv(url_unemployment) |> dplyr::select(entity, year,sl_uem_totl_zs) |> dplyr::rename(country = entity, unemployment = sl_uem_totl_zs)
 
-df <- dplyr::inner_join(x=productivity, y=unemployment, by=dplyr::join_by(country, year))
+url_gdp <- "https://ourworldindata.org/grapher/annual-working-hours-vs-gdp-per-capita-pwt.csv?v=1&csvType=full&useColumnShortNames=true"
+gdp <-  read.csv(url_gdp) |> dplyr::select(entity, year,rgdpo_pc) |> dplyr::rename(country = entity, gdp = rgdpo_pc)
+
+productivity_unemployment <- dplyr::inner_join(x=productivity, y=unemployment, by=dplyr::join_by(country, year))
+
+df <- dplyr::inner_join(x=productivity_unemployment, y=gdp, by=dplyr::join_by(country, year))
 
 

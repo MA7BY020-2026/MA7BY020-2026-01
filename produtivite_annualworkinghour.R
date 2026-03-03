41b628111a858dbb5825f583ce1a9a9558305693
huangl <supershim@icloud.com>
2026-03-03 19:03:42 +0100

    Add a plot showing one year of data

---
+++ produtivite_annualworkinghour.R
@@ -0,0 +1,27 @@
+
+library(dplyr)
+
+url_productivity <- "https://ourworldindata.org/grapher/labor-productivity-per-hour-pennworldtable.csv?v=1&csvType=full&useColumnShortNames=true"
+productivity <-  read.csv(url_productivity) |> dplyr::select(entity, year,productivity) |> dplyr::rename(country = entity)
+
+url_workhour <- "https://ourworldindata.org/grapher/annual-working-hours-per-worker.csv?v=1&csvType=full&useColumnShortNames=true"
+workhour <-read.csv(url_workhour) |> dplyr::select(entity, year,working_hours_omm) |> dplyr::rename(country = entity, workhour = working_hours_omm)
+
+url_population <- "https://ourworldindata.org/grapher/population.csv?v=1&csvType=full&useColumnShortNames=true"
+population <-  read.csv(url_population) |> dplyr::select(entity, year,population_historical) |> dplyr::rename(country = entity, population = population_historical)
+
+productivity_workhour <- dplyr::inner_join(x=productivity, y=workhour, by=dplyr::join_by(country, year))
+
+df <- dplyr::inner_join(x=productivity_workhour, y=population, by=dplyr::join_by(country, year))
+
+figure <- df |>
+  dplyr::filter(year==2023) |>
+  ggplot() +
+  aes(x = productivity) +
+  aes(y = workhour) +
+  aes(fill = country) +
+  aes(color= country) +
+  aes(frame = year) +
+  geom_point(alpha=.5)
+
+print(figure)

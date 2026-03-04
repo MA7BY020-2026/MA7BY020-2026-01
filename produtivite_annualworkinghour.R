461f3bed43b4f702a03a73ced0cf77cb7b2d6678
huangl <supershim@icloud.com>
2026-03-04 23:10:41 +0100

    comment

--- produtivite_annualworkinghour.R
+++ produtivite_annualworkinghour.R
@@ -4,28 +4,38 @@ library(countrycode)
 library(gapminder)
 library(plotly)
 
+# Download and make a clean dataframe from ourworldindata's productivity data 
 url_productivity <- "https://ourworldindata.org/grapher/labor-productivity-per-hour-pennworldtable.csv?v=1&csvType=full&useColumnShortNames=true"
 productivity <-  read.csv(url_productivity) |> dplyr::select(entity, year,productivity) |> dplyr::rename(country = entity)
 
+# Same with working hours data
 url_workhour <- "https://ourworldindata.org/grapher/annual-working-hours-per-worker.csv?v=1&csvType=full&useColumnShortNames=true"
 workhour <-read.csv(url_workhour) |> dplyr::select(entity, year,working_hours_omm) |> dplyr::rename(country = entity, workhour = working_hours_omm)
 
+# Same with population data
 url_population <- "https://ourworldindata.org/grapher/population.csv?v=1&csvType=full&useColumnShortNames=true"
 population <-  read.csv(url_population) |> dplyr::select(entity, year,population_historical) |> dplyr::rename(country = entity, population = population_historical)
+# Now we have three datasets
 
+# We merge the productivity and working hours dataframes into one with inner_join(). Inner means empty (NA) observations/rows are discarded
 productivity_workhour <- dplyr::inner_join(x=productivity, y=workhour, by=dplyr::join_by(country, year))
+# Now we have a dataset with the following columns/variables: country, year, productivity, working_hours_omm
 
+
+# We merge the (productivity+working hours) and population dataframes. Here also, inner_join() to remove observations with empty values.
+# We also use the countrycode library to add a column with continent of each observation (to make the figure more beautiful)
 df <- dplyr::inner_join(x=productivity_workhour, y=population, 
                         by=dplyr::join_by(country, year)) |>
      dplyr::mutate(
      continent = countrycode(country, origin = "country.name", destination = "continent")
      ) |>
-     dplyr::filter(!is.na(continent)) |>           # ← 
+     dplyr::filter(!is.na(continent)) |>           
      dplyr::group_by(year) |>
-     dplyr::filter(n_distinct(continent) == 5) |>
+     dplyr::filter(n_distinct(continent) == 5) |>    # We only keep years where all 5 continents are represented
      dplyr::ungroup() |>
      dplyr::arrange(country, year) 
 
+# We make the figure
 figure <- df |>
   arrange(year, desc(population)) |> 
   ggplot(aes(x = productivity, 
